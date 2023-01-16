184d7522128a1542e124f4ab6d084d1386291a9b
JSergio86 <sjimenezg@elpuig.xeill.net>
2023-01-16 17:09:13 +0100

    Cambios avanzados

--- src/main/java/com/example/practicajavafx/Controlador.java
+++ src/main/java/com/example/practicajavafx/Controlador.java
@@ -15,7 +15,7 @@ import java.util.Arrays;
 import java.util.ResourceBundle;
 
 //implements Initializable para cuando se inicie el xml iniciar lo que pongamos en el metodo
-public class Controlador implements Initializable {
+public class Controlador {
     @FXML
     Button b0;
     @FXML
@@ -38,8 +38,6 @@ public class Controlador implements Initializable {
     Button bc;
     @FXML
     private Text winnerText;
-
-    private int playerTurn = 0;
     @FXML
     RadioButton computervscomputer;
     @FXML
@@ -47,103 +45,120 @@ public class Controlador implements Initializable {
     @FXML
     RadioButton humanvscomputer;
 
+
     @FXML
     ArrayList<Button> buttons = new ArrayList<>();
 
     int idModo = 0;
+
+    boolean turnoJugador = false;
+    boolean turnoIA = false;
+
+    boolean comenzarJuego=false;
+
+
     public void comprobarModo(){
         if(humanvshuman.isSelected()){
             idModo=0;
+
         }
         if(computervscomputer.isSelected()){
             idModo=1;
-            System.out.println("Modo computervscompter");
         }
         if(humanvscomputer.isSelected()){
             idModo=2;
+            System.out.println("Modo humanvscompter");
+
         }
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        comprobarModo();
-
-        switch (idModo){
-            case 0:
-                HumanVSHuman();
-                break;
-        }
-    }
-
-    public void HumanVSHuman(){
-
-    }
 
     public void Marcar(ActionEvent event){
-        boolean x = false;
-        boolean o = false;
-
         bc = (Button) event.getSource();
         String sid = bc.getId().replaceAll("[b]","");
         int id =Integer.valueOf(sid);
 
-        if(bc.isPressed()){
-            x=true;
-            bc.setText("X");
-            System.out.println("X true");
-        }
+            if(comenzarJuego== true){
+                turnoJugador=false;
+                comenzarJuego=false;
+            }
 
-        if(bc.isPressed()){
-            o=true;
-            bc.setText("O");
-            System.out.println("Y true");
-        }
+            if(idModo==0){
+                HumanVSHuman();
+            }
+
+            if(idModo==1){
+                ComputerVSComputer();
+            }
+
+            if(idModo==2){
+
+            }
 
 
+        comprobarGanador();
     }
 
-    @FXML
-    void restartGame(ActionEvent event) {
-        buttons.forEach(this::resetButton);
-        winnerText.setText("Tres en raya");
-    }
-    public void resetButton(Button button) {
-        button.setDisable(false);
-        button.setText("");
-    }
-     /*public void HumanVSHuman(){
-        buttons = new ArrayList<>(Arrays.asList(b0,b1, b2, b3, b4, b5, b6, b7, b8));
-        buttons.forEach(button -> {
-            setupButton(button);
-            button.setFocusTraversable(false);
-        });
-    }
+    public void HumanVSHuman(){
+        for(int i=0;i<1;i++) {
+            if(bc.getText() == "X" || bc.getText() == "O"){
+                winnerText.setText("No esta permitido");
+                break;
+            }
 
+            if (turnoJugador == false) {
+                bc.setText("X");
+                turnoJugador = true;
+                break;
 
+            }
 
-
-
-    private void setupButton(Button button) {
-        button.setOnMouseClicked(mouseEvent -> {
-            setPlayerSymbol(button);
-            button.setDisable(true);
-            checkIfGameIsOver();
-        });
-    }
-
-    public void setPlayerSymbol(Button button) {
-        if (playerTurn % 2 == 0) {
-            button.setText("X");
-            playerTurn = 1;
-        } else {
-            button.setText("O");
-            playerTurn = 0;
+            if (turnoJugador == true) {
+                turnoJugador = false;
+                bc.setText("O");
+                break;
+            }
         }
     }
 
-    public void checkIfGameIsOver() {
-        for (int a = 0; a < 8; a++) {
-            String line = switch (a) {
+    public void ComputerVSComputer(){
+        //Añadir botones al array
+            buttons.add(b0);
+            buttons.add(b1);
+            buttons.add(b2);
+            buttons.add(b3);
+            buttons.add(b4);
+            buttons.add(b5);
+            buttons.add(b6);
+            buttons.add(b7);
+            buttons.add(b8);
+
+
+        int botonIA = (int) (Math.random()*8+0);
+
+        for(int i=0;i<1;i++) {
+            //No funciona bien cambiar bc por buttons.get(botonIA).setText("X");
+            if(bc.getText() == "X" || bc.getText() == "O"){
+                break;
+            }
+
+            if (turnoIA == false) {
+                buttons.get(botonIA).setText("X");
+                turnoIA = true;
+                break;
+            }
+
+            if (turnoIA == true) {
+                buttons.get(botonIA).setText("O");
+                turnoIA = false;
+                break;
+            }
+        }
+    }
+
+    public void comprobarGanador() {
+        for (int i = 0; i < 8; i++) {
+            String linea = switch (i) {
                 case 0 -> b0.getText() + b1.getText() + b2.getText();
                 case 1 -> b3.getText() + b4.getText() + b5.getText();
                 case 2 -> b6.getText() + b7.getText() + b8.getText();
@@ -155,18 +170,61 @@ public class Controlador implements Initializable {
                 default -> null;
             };
 
-            //X winner
-            if (line.equals("XXX")) {
-                winnerText.setText("X won!");
-                return;
+
+            if (linea.equals("XXX")) {
+                winnerText.setText("¡Gano X!");
+
+                b0.setDisable(true);
+                b1.setDisable(true);
+                b2.setDisable(true);
+                b3.setDisable(true);
+                b4.setDisable(true);
+                b5.setDisable(true);
+                b6.setDisable(true);
+                b7.setDisable(true);
+                b8.setDisable(true);
+
             }
 
-            //O winner
-            else if (line.equals("OOO")) {
-                winnerText.setText("O won!");
-                return;
+            else if (linea.equals("OOO")) {
+                winnerText.setText("¡Gano O!");
+
+                b0.setDisable(true);
+                b1.setDisable(true);
+                b2.setDisable(true);
+                b3.setDisable(true);
+                b4.setDisable(true);
+                b5.setDisable(true);
+                b6.setDisable(true);
+                b7.setDisable(true);
+                b8.setDisable(true);
+
             }
         }
+    }
 
-  */
+    public void comenzarJuego() {
+        b0.setDisable(false);
+        b1.setDisable(false);
+        b2.setDisable(false);
+        b3.setDisable(false);
+        b4.setDisable(false);
+        b5.setDisable(false);
+        b6.setDisable(false);
+        b7.setDisable(false);
+        b8.setDisable(false);
+
+        b0.setText("");
+        b1.setText("");
+        b2.setText("");
+        b3.setText("");
+        b4.setText("");
+        b5.setText("");
+        b6.setText("");
+        b7.setText("");
+        b8.setText("");
+
+        comenzarJuego = true;
+    }
+
     }
